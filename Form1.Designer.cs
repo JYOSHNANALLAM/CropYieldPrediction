18e689081298da528bbc1557770efb6802688fb7
JYOSHNANALLAM <jyoshna0801@gmail.com>
2025-11-28 12:36:14 +0530

    Uploading project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CropYieldPrediction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(532, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(132, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "btnBrowse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click_1);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(622, 85);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(110, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "btnImport";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click_1);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(48, 63);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 62;
+            this.dgvData.RowTemplate.Height = 28;
+            this.dgvData.Size = new System.Drawing.Size(240, 150);
+            this.dgvData.TabIndex = 4;
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(459, 88);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(82, 20);
+            this.lblFilePath.TabIndex = 5;
+            this.lblFilePath.Text = "lblFilePath";
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(506, 125);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(100, 26);
+            this.txtFilePath.TabIndex = 6;
+            this.txtFilePath.Text = "txtFilePath";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblFilePath);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,22 @@
+# Crop Yield Prediction Tool
+
+A Windows Forms application built in C# for importing crop data from Excel and storing it in SQLite.
+
+## Features
+- Browse and import Excel files
+- Store data in SQLite database
+- View imported data in a grid
+- Responsive layout with dynamic resizing
+- Navigation between forms
+
+## How to run
+1. Open `CropYieldPrediction.sln` in Visual Studio
+2. Press F5 to run
+3. Browse and select an Excel file
+4. Click Import to load data into SQLite
+
+## Sample data
+Place your Excel file in `Assets/sample_data.xlsx`.
+
+## Author
+Jyoshna Nallam
